bd8bb01d0bed0daa401115e7449481fd7c5fba09
sathyablaze <jhonnyblaze2002@gmail.com>
2023-02-04 02:07:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Product Search'),
+        ),
+      ),
+    );
+  }
+}
